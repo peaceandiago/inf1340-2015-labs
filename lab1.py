bc6bb7b4ac4bd8513bb6d6cf35299a4d88f9b80b
peaceandiago <paola.santiago@mail.utoronto.ca>
2015-10-01 12:30:29 -0400

    Solved

--- lab1.py
+++ lab1.py
@@ -18,3 +18,18 @@ def vowel_or_consonant():
     display a message indicating that the letter is a "consonant".
     """
 
+    letter = raw_input("Enter a letter\n")
+    if letter == "a":
+        print ("vowel")
+    elif letter == "e":
+        print ("vowel")
+    elif letter == "i":
+        print ("vowel")
+    elif letter == "o":
+        print ("vowel")
+    elif letter == "u":
+        print ("vowel")
+    elif letter == "y":
+        print ("sometimes a vowel, sometimes a consonant")
+    else:
+        print ("consonant")
